ebbf5cd8842080cb6aa7b16cb7b405e96fd315cf
alimasudd <alimasudd.dev@gmail.com>
2023-12-07 09:16:13 +0700

    modified colors.dart

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_flutter_firebase/colors.dart';
+import 'package:whatsapp_flutter_firebase/common/utils/colors.dart';
 import 'package:whatsapp_flutter_firebase/info.dart';
 import 'package:whatsapp_flutter_firebase/screens/mobile_chat_screen.dart';
 
