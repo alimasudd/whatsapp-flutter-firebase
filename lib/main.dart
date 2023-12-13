d8ed94c55d36449909cd94eb32daa4807ff83084
alimasudd <alimasudd.dev@gmail.com>
2023-12-13 09:01:06 +0700

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsapp_flutter_firebase/colors.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_flutter_firebase/common/utils/colors.dart';
+import 'package:whatsapp_flutter_firebase/common/widgets/error.dart';
+import 'package:whatsapp_flutter_firebase/common/widgets/loader.dart';
+import 'package:whatsapp_flutter_firebase/features/landing/screens/landing_screen.dart';
+import 'package:whatsapp_flutter_firebase/router.dart';
 import 'package:whatsapp_flutter_firebase/screens/mobile_layout_screen.dart';
-import 'package:whatsapp_flutter_firebase/screens/web_layout_screen.dart';
-import 'package:whatsapp_flutter_firebase/utils/responsive_layout.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'features/auth/controller/auth_controller.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Whatsapp UI',
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: backgroundColor,
+          scaffoldBackgroundColor: backgroundColor,
+          appBarTheme: const AppBarTheme(
+            color: appBarColor,
+          ),
       ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileLayoutScreen(),
-        webScreenLayout: WebLayoutScreen(),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: ref.watch(userDataAuthProvider).when(
+        data: (user) {
+          if (user == null) {
+            return const LandingScreen();
+          }
+          return const MobileLayoutScreen();
+        },
+        error: (err, trace) {
+          return ErrorScreen(
+            error: err.toString(),
+          );
+        },
+        loading: () => const Loader(),
       ),
     );
   }
