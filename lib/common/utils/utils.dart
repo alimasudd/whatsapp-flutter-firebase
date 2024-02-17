fab9d82e301ea9f126883321b97580df85f36e35
alimasudd <alimasudd.dev@gmail.com>
2024-02-17 20:38:34 +0700

    modified utils.dart

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:enough_giphy_flutter/enough_giphy_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -40,15 +41,15 @@ Future<File?> pickVideoFromGallery(BuildContext context) async {
   return video;
 }
 
-// Future<GiphyGif?> pickGIF(BuildContext context) async {
-//   GiphyGif? gif;
-//   try {
-//     gif = await Giphy.getGif(
-//       context: context,
-//       apiKey: 'pwXu0t7iuNVm8VO5bgND2NzwCpVH9S0F',
-//     );
-//   } catch (e) {
-//     showSnackBar(context: context, content: e.toString());
-//   }
-//   return gif;
-// }
+Future<GiphyGif?> pickGIF(BuildContext context) async {
+  GiphyGif? gif;
+  try {
+    gif = await Giphy.getGif(
+      context: context,
+      apiKey: 'pwXu0t7iuNVm8VO5bgND2NzwCpVH9S0F',
+    );
+  } catch (e) {
+    showSnackBar(context: context, content: e.toString());
+  }
+  return gif;
+}
