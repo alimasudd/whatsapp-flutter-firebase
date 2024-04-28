5cdcea3c200decbf1f49041dd52ddd8056e9ecef
alimasudd <alimasudd.dev@gmail.com>
2024-04-28 15:47:19 +0700

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_flutter_firebase/common/utils/colors.dart';
 import 'package:whatsapp_flutter_firebase/common/widgets/error.dart';
 import 'package:whatsapp_flutter_firebase/common/widgets/loader.dart';
+import 'package:whatsapp_flutter_firebase/features/auth/controller/auth_controller.dart';
 import 'package:whatsapp_flutter_firebase/features/landing/screens/landing_screen.dart';
+import 'package:whatsapp_flutter_firebase/firebase_options.dart';
 import 'package:whatsapp_flutter_firebase/router.dart';
-import 'package:whatsapp_flutter_firebase/screens/mobile_layout_screen.dart';
-
-import 'features/auth/controller/auth_controller.dart';
-import 'firebase_options.dart';
+import 'package:whatsapp_flutter_firebase/mobile_layout_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,26 +31,26 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: 'Whatsapp UI',
       theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: backgroundColor,
-          appBarTheme: const AppBarTheme(
-            color: appBarColor,
-          ),
+        scaffoldBackgroundColor: backgroundColor,
+        appBarTheme: const AppBarTheme(
+          color: appBarColor,
+        ),
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: ref.watch(userDataAuthProvider).when(
-        data: (user) {
-          if (user == null) {
-            return const LandingScreen();
-          }
-          return const MobileLayoutScreen();
-        },
-        error: (err, trace) {
-          return ErrorScreen(
-            error: err.toString(),
-          );
-        },
-        loading: () => const Loader(),
-      ),
+            data: (user) {
+              if (user == null) {
+                return const LandingScreen();
+              }
+              return const MobileLayoutScreen();
+            },
+            error: (err, trace) {
+              return ErrorScreen(
+                error: err.toString(),
+              );
+            },
+            loading: () => const Loader(),
+          ),
     );
   }
 }
